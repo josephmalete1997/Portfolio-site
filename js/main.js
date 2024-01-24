b4e1bc9a3868cd440744175ed3026cab236ada39
josephmalete1997 <josephmalete75@gmail.com>
2024-01-24 16:33:32 +0200

    Adding portfolio projects

--- js/main.js
+++ js/main.js
@@ -27,3 +27,39 @@ inside[1].addEventListener("click", () => {
 
   body.style.overflow = "auto";
 });
+
+// CV download
+
+const loadDownload = document.querySelectorAll(".btn-load");
+
+document.getElementById("download").addEventListener("click", () => {
+  loadDownload[0].style.visibility = "visible";
+  setTimeout(() => {
+    loadDownload[0].style.visibility = "hidden";
+    download();
+  }, 1500);
+});
+
+function download() {
+  const pdfUrl = "Joseph_malete(Updated_CV).pdf";
+  const anchor = document.createElement("a");
+
+  anchor.href = pdfUrl;
+  anchor.download = "Joseph Malete Updated CV.pdf";
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+}
+// View CV
+document.getElementById("view").addEventListener("click", () => {
+  loadDownload[1].style.visibility = "visible";
+  setTimeout(() => {
+    loadDownload[1].style.visibility = "hidden";
+    view();
+  }, 2500);
+});
+
+function view() {
+  const pdfUrl = "Joseph_malete(Updated_CV).pdf";
+  window.open(pdfUrl, "_blank");
+}
